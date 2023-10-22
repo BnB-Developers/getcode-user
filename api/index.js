dc38affc89ff54a0c9eb88b662e32302dd8656f8
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-10-22 21:26:37 +0530

    hehe

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const uri = process.env.MONGODB_URI
+const uri = "mongodb+srv://vercel-admin-user:RpUpV6Tp4oZk6Mjr@cluster0.fjnp4qu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,10 +33,6 @@ app.get('/api/:name', async (req, res) => {
     res.setHeader('Content-Type','application/json');
     res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
     res.send(data)
-})
-
-app.listen(8000,()=>{
-    console.log('listening to port 3000')
 })
 
 module.exports = app
