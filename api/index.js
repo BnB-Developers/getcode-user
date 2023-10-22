d709dbfb64854ab5e2f888c1a0b4b36b6e81df9a
vansh845 <103567533+vansh845@users.noreply.github.com>
2023-10-22 17:42:16 +0530

    api created

--- api/index.js
+++ api/index.js
@@ -1,8 +1,31 @@
 const express = require('express')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+const uri = process.env.MONGO_URI
 
-app.get('/api',(req,res)=>{
-    res.send({message:'hello',success:false})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    await client.connect();        
+}
+run()
+
+app.get('/api', async (req, res) => {
+    const myDb = client.db('user')
+    const myColl = myDb.collection('data')
+    const data =  await myColl.findOne({})
+    console.log(data)
+    res.send(data)
 })
+
+app.listen(3000)
 
 module.exports = app
